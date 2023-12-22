426b33172742e36f2d311352a10f2d89087f5d97
itsayushagrawal <agaayush15@gmail.com>
2023-12-22 20:35:43 +0530

    Final Commit

--- app/src/main/java/com/example/rockerpaperscissors/RockPaperScissors.kt
+++ app/src/main/java/com/example/rockerpaperscissors/RockPaperScissors.kt
@@ -3,34 +3,50 @@ package com.example.rockerpaperscissors
 fun main(){
 
     var computerChoice=""
-    //var playerChoice=""
-
-    println("Rock, Paper, Scissors?")
-    print("Enter your choice: ")
-
-    val playerChoice=readln().lowercase()
+    var playerChoice=""
 
 
 
-    val randomNumber=(1..3).random()
 
-    when(randomNumber) {
-        1-> computerChoice="rock"
-        2-> computerChoice="paper"
-        3-> computerChoice="scissor"
+    while(true) {
+
+        println("Rock, Paper, Scissor or Exit?")
+        print("Enter your choice: ")
+        playerChoice=readln().lowercase()
+
+        if (playerChoice == "rock" || playerChoice == "paper" || playerChoice == "scissor") {
+
+            val randomNumber = (1..3).random()
+
+            when (randomNumber) {
+                1 -> computerChoice = "rock"
+                2 -> computerChoice = "paper"
+                3 -> computerChoice = "scissor"
+            }
+
+            println("Computer Chose $computerChoice")
+
+            val winner = when {
+                playerChoice == computerChoice -> "TIE"
+
+                playerChoice == "rock" && computerChoice == "scissor" -> "Player Won!"
+                playerChoice == "paper" && computerChoice == "rock" -> "Player Won!"
+                playerChoice == "scissor" && computerChoice == "paper" -> "Player Won!"
+
+                else -> "Computer"
+            }
+
+            println(winner)
+            println()
+        }
+
+        else if (playerChoice == "exit") {
+            break
+        }
+
+        else {
+            println("\nEnter valid input!!")
+        }
     }
 
-    println("Computer Chose "+computerChoice)
-
-    val winner= when {
-        playerChoice==computerChoice-> "TIE"
-
-        playerChoice=="rock" && computerChoice == "scissor" -> "Player Won!"
-        playerChoice=="paper" && computerChoice == "rock" -> "Player Won!"
-        playerChoice=="scissor" && computerChoice == "paper" -> "Player Won!"
-
-        else -> "Computer"
-    }
-
-    println(winner)
 }
